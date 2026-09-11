356e3d61383ec40bff752ea73415f0b52bbd07a8
Margaret Cyr <margaret.cyr217@gmail.com>
2026-09-11 15:12:52 -0400

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/maggie/seamstock/SeamstockApplication.java
@@ -0,0 +1,13 @@
+package com.maggie.seamstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeamstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeamstockApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/maggie/seamstock/SeamstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maggie.seamstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeamstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
